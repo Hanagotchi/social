9ce1e95a1ac31d2ca3a59b8941ffba17acdd5d05
Federico Pacheco <fpacheco@fi.uba.ar>
2024-04-20 15:23:56 -0300

    fix jwtbearer

--- app/security/JWTBearer.py
+++ app/security/JWTBearer.py
@@ -1,12 +1,12 @@
-import os
 from typing import Annotated
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi import Request, Depends, HTTPException, status
 from jose import JWTError, jwt
 from pydantic import BaseModel
+from os import environ
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+JWT_SECRET = environ.get("JWT_SECRET")
+HASH_ALGORITHM = environ.get("HASH_ALGORITHM")
 TOKEN_FIELD_NAME = "x-access-token"
 
 
@@ -22,7 +22,7 @@ class JWTBearer(HTTPBearer):
             ):
                 raise HTTPException(
                     status_code=status.HTTP_401_UNAUTHORIZED,
-                    detail="Invalid authentication scheme or token.",
+                    detail="(Bearer) Invalid scheme or token.",
                 )
             return credentials.credentials
 
@@ -33,14 +33,14 @@ class JWTBearer(HTTPBearer):
 
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid authorization code or token.",
+                detail="(x-access-token) Invalid scheme or token.",
             )
 
     def verify_jwt(self, jwtoken: str) -> bool:
         isTokenValid: bool = False
 
         try:
-            payload = jwt.decode(jwtoken, SECRET_KEY, algorithms=ALGORITHM)
+            payload = jwt.decode(jwtoken, JWT_SECRET, algorithms=HASH_ALGORITHM)
         except Exception:
             payload = None
         if payload:
@@ -59,7 +59,7 @@ async def get_current_user_id(token: Annotated[dict, Depends(JWTBearer())]):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, JWT_SECRET, algorithms=[HASH_ALGORITHM])
         user_id: int = payload.get("user_id")
         if user_id is None:
             raise credentials_exception
